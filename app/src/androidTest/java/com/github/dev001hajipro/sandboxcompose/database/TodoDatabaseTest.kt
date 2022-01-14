2ba58d1ef6e73bcfc8984064d7897e6760d951a2
dev001hajipro <dev001hajipro@gmail.com>
2022-01-14 17:35:25 +0900

    test - Adding missing tests for update

--- app/src/androidTest/java/com/github/dev001hajipro/sandboxcompose/database/TodoDatabaseTest.kt
+++ app/src/androidTest/java/com/github/dev001hajipro/sandboxcompose/database/TodoDatabaseTest.kt
@@ -41,4 +41,19 @@ class TodoDatabaseTest {
         val x = dao.getById(1)
         assertEquals(x?.itemId, 1L)
     }
+
+    @Test
+    @Throws(Exception::class)
+    fun updateAndGetTodo() = runBlocking {
+        val item = TodoItem(itemId = 1, name = "Dummy", isDone = false)
+        dao.insert(item)
+        val x = dao.getById(1)
+        if (x != null) {
+            x.isDone = true
+            dao.update(x)
+
+            assertEquals(dao.getById(1)?.isDone, true)
+        }
+
+    }
 }
